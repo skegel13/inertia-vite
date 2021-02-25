fea441e5260beb2b05d3e04bda12d179476c7657
Sean Kegel <sean.kegel@hey.com>
2021-02-24 21:57:27 -0500

    Update app.js to handle Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,41 @@
-require('./bootstrap');
+import "vite/dynamic-import-polyfill";
+import { createApp, h } from "vue";
+import {
+  App as InertiaApp,
+  plugin as InertiaPlugin,
+} from "@inertiajs/inertia-vue3";
+import { InertiaProgress } from "@inertiajs/progress";
 
-// Import modules...
-import { createApp, h } from 'vue';
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
+// Import Axios and set config here instead of bootstrap file.
+import axios from "axios";
 
-const el = document.getElementById('app');
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+const el = document.getElementById("app");
+
+// Get all the page components. This is required for the Rollup production
+// build.
+const pages = import.meta.glob("./Pages/**/*.vue");
 
 createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
-})
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .mount(el);
+  render: () =>
+    h(InertiaApp, {
+      initialPage: JSON.parse(el.dataset.page),
+      // Update the resolve component to use the import syntax. The path should
+      // begin with a './' instead of using the alias.
+      // https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations
+      resolveComponent: (name) => {
+        const page = pages[`./Pages/${name}.vue`];
+        if (!page) {
+          throw new Error(`Page ${name} not found.`);
+        }
 
-InertiaProgress.init({ color: '#4B5563' });
+        return page().then((module) => module.default);
+      },
+    }),
+})
+  .mixin({ methods: { route } })
+  .use(InertiaPlugin)
+  .mount(el);
+
+InertiaProgress.init({ color: "#4B5563" });
