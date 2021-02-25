5de0363fb3f386735f27345a35395b70db41f2f8
Sean Kegel <sean.kegel@hey.com>
2021-02-24 21:13:22 -0500

    Update app layout to include Vite assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,38 @@
+@php
+if (config('app.env') == 'production') {
+    // If we are in production, we need to parse the manifest file generated
+    // by the Vite build process.
+    $manifest = json_decode(@file_get_contents(public_path('/dist/manifest.json')), true);
+}
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Scripts -->
+    @routes
+
+    @if (config('app.env') == 'production')
+        <link rel="stylesheet" href="dist/{{ Arr::get($manifest['js/app.js'], 'css.0') }}" />
+        <script type="module" src="dist/{{ Arr::get($manifest['js/app.js'], 'file') }}" defer></script>
+    @else
+        <script type="module" src="http://localhost:{{ config('app.vite_port', 3000) }}/dist/@vite/client"></script>
+        <script type="module" src="http://localhost:{{ config('app.vite_port', 3000) }}/dist/js/app.js"></script>
+    @endif
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
